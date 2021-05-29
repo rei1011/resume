e78e2eae73b7638e80267fd41e5ef8d104cf6ea6
buchirei <r.mizobuchi3@gmail.com>
2021-05-30 02:46:36 +0900

    add animation

--- src/page/works/Works.tsx
+++ src/page/works/Works.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ContentsWrapper from '../../common/ContentsWrapper';
 import WorkDetail from './WorkDetail';
 import WorkList from './WorkList';
+import throttle from "lodash/throttle";
 
 
 export type work = {
@@ -19,13 +20,38 @@ let workJson: workJson
 
 const Works = () => {
   const [workIndex, setWorkIndex] = useState(0);
+  const [isFadeOut, setIsFadeOut] = useState(false);
+  const [isFadeIn, setIsFadeIn] = useState(false);
+  const [typeOfChange, setTypeOfChange] = useState("");
+  const changeWorkIndex = (index: number) => {
+    setIsFadeOut(true);
+    if(workIndex < index) {
+      setTypeOfChange("top-to-bottom");
+    } else if (workIndex > index) {
+      setTypeOfChange("bottom-to-top");
+    }
+    setTimeout(
+      () => {
+        setWorkIndex(index);
+        setIsFadeOut(false);
+        setIsFadeIn(true);
+        setTimeout(() => {
+          setIsFadeIn(false);
+          setTypeOfChange("");
+        }, 400);
+      },
+      400);
+  };
+  
+
   return (
     <ContentsWrapper>
-      <div className={"works-wrapper flex fit"}>  
+      <div className={getClassName(isFadeOut, isFadeIn, typeOfChange)}>  
         <WorkList works={workJson.works} 
                   workIndex={workIndex} 
-                  setWorkIndex={(index: number) => setWorkIndex(index)}/>
-        <WorkDetail work={workJson.works[workIndex]} />
+                  changeWorkIndex={changeWorkIndex}/>
+        <WorkDetail works={workJson.works} 
+                    workIndex={workIndex} />
       </div>
     </ContentsWrapper>
   );
@@ -39,4 +65,17 @@ export const getWorksData = () => {
           .then(resJson => {
             workJson=resJson
           });
+}
+
+
+const getClassName = (isFadeOut: boolean, isFadeIn: boolean, typeOfChange: string) => {
+  const classNameList = ["works-wrapper", "flex", "fit", `${typeOfChange}`];
+  if(isFadeOut) {
+    classNameList.push("fade-out");
+  }
+  if(isFadeIn) {
+    classNameList.push("fade-in");
+  }
+
+  return classNameList.join(" ");
 }
